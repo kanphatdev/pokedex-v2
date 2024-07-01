f2bb539af20a09da6d8988c086d8fd15873240bd
kanphatratrazer <kanphatratrazer28@gmail.com>
2024-07-01 10:00:49 +0700

    add ui state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Board";
 import Navbar from "./components/Navbar";
 
 function App() {
--- src/components/pokemon/PokemonCards.jsx
+++ src/components/pokemon/PokemonCards.jsx
@@ -1,26 +1,34 @@
-
-
-const PokemonCards = ({ pokemon,index }) => {
+import PropTypes from 'prop-types'; 
+export const PokemonCards = ({ pokemon, index }) => {
   return (
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-    <img src={pokemon?.sprites?.other?.home?.front_default } />
-    </a>
-    <div className="p-5">
-        <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {pokemon.name}
-            </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-           {pokemon.id}
+    <div
+      key={index}
+      className="p-4 bg-white border border-gray-200 rounded-lg shadow"
+    >
+      <div className="mb-4">
+        <div className="text-gray-600">{index}</div>
+      </div>
+      <div className="mb-4">
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            index + 1
+          }.png`}
+          alt={pokemon.name}
+          className="w-full h-auto object-contain"
+        />
+      </div>
+      <div>
+        {/* Add other Pokemon details here if available */}
+        <p className="text-gray-700 text-xl font-bold mb-2 capitalize text-center">
+          {pokemon.name}
         </p>
-      
+      </div>
     </div>
-</div>
+  );
+};
 
-
-  )
+export default PokemonCards;
+PokemonCards.propTypes ={
+  pokemon:PropTypes.string,
+  index:PropTypes.any
 }
-
-export default PokemonCards
